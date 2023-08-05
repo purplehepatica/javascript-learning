0cebf5801aa2d9ac881d38cf64fa3f1a7080dbc6
Jakub <102424460+purplehepatica@users.noreply.github.com>
2023-08-05 21:48:13 +0200

    Updates and Fun

--- css-playground/to-do/task-list-pico/script.js
+++ css-playground/to-do/task-list-pico/script.js
@@ -25,17 +25,81 @@ function createTaskComponent() {
         <article class="task-container">
             <div class="grid">
                 <p>${getTaskInputData()}</p>
+                <button class="open-task-button button contrast">Otwórz</button>
                 <button class="edit-task-button button secondary" data-is-edited="false">Edytuj</button>
                 <button class="remove-task-button button is-light">Zrobione</button>
             </div>
-            
+            <dialog>
+                <article>
+                    <button
+                       class="close">
+                    </button>
+                    <h3 class="task-title">${getTaskInputData()}</h3>
+                    
+                    <p class="description">
+                        Cras sit amet maximus risus.
+                        Pellentesque sodales odio sit amet augue finibus pellentesque.
+                        Nullam finibus risus non semper euismod.
+                    </p>
+                    
+                    <div class="grid">
+                        <input class="subtask-input-field" placeholder="Kupić..." />
+                        <button class="add-subtask-button">Dodaj zadanie</button>
+                        <button class="clear-subtasks-data secondary">Wyczyść zadania</button>
+                    </div>
+                    
+                    <div class="subtasks">
+                    
+                    </div>
+                    
+                    
+                </article>
+            </dialog>
         </article>
     `;
+
+    const subtaskInput = taskContainer.querySelector(".subtask-input-field");
+    const addSubtaskButton = taskContainer.querySelector(".add-subtask-button");
+    const clearSubtasksData = taskContainer.querySelector(".clear-subtasks-data");
+    const subtasks = taskContainer.querySelector(".subtasks");
+
+
+    addSubtaskButton.addEventListener("click", () => {
+        createSubtaskComponent(subtaskInput, addSubtaskButton, clearSubtasksData, subtasks);
+        subtaskInput.value = "";
+    })
+
+    taskContainer.querySelector(".clear-subtasks-data").addEventListener("click", () => {
+
+        taskContainer.querySelector(".subtasks").innerHTML = "";
+        localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML);
+    });
+
+    subtaskInput.addEventListener("keydown", (e) => {
+
+        if (e.key === "Enter") {
+            createSubtaskComponent(subtaskInput, addSubtaskButton, clearSubtasksData, subtasks);
+            subtaskInput.value = "";
+        }
+    })
 
     taskContainer.querySelector(".remove-task-button").addEventListener("click", () => {
         taskContainer.remove();
             localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML)
         });
+
+
+    const modal = taskContainer.querySelector("dialog");
+    const closeModalButton = taskContainer.querySelector("dialog .close");
+
+    taskContainer.querySelector(".open-task-button").addEventListener("click", () => {
+        modal.setAttribute("open", "true");
+    })
+
+    closeModalButton.addEventListener("click", () => {
+        modal.removeAttribute("open");
+    })
+
 
 
 
@@ -57,10 +121,95 @@ function createTaskComponent() {
         elements.mainTasksContainer.append(taskContainer);
     }
 
+
+
     localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML);
 
     elements.taskInputField.value = null;
 }
+
+
+function createSubtaskComponent(subtaskInput, addSubtaskButton, clearSubtasksData, subtasks) {
+
+    const subtaskContainer = document.createElement("div");
+    subtaskContainer.classList.add("subtask");
+
+    subtaskContainer.innerHTML = `
+        <article class="article subtask-container">
+            <div class="grid">
+                <p class="subtask-title">${subtaskInput.value}</p>
+                <button class="edit-subtask-button button secondary" data-is-edited="false">Edytuj</button>
+                <button class="remove-subtask-button button is-light">Zrobione</button>
+            </div>
+        </article>
+    `;
+
+    clearSubtasksData.addEventListener("click", () => {
+        subtasks.innerHTML = "";
+    })
+
+    subtaskContainer.querySelector(".remove-subtask-button").addEventListener("click", () => {
+        subtaskContainer.remove();
+    })
+
+    subtaskContainer.querySelector(".edit-subtask-button").addEventListener("click", () => {
+
+        if (subtaskContainer.querySelector(".edit-subtask-button").dataset.isEdited === "false") {
+            subtaskContainer.querySelector(".edit-subtask-button").dataset.isEdited = "true";
+            subtaskEditModeOn(subtaskContainer);
+        } else if (subtaskContainer.querySelector(".edit-subtask-button").dataset.isEdited === "true") {
+            subtaskContainer.querySelector(".edit-subtask-button").dataset.isEdited = "false";
+            subtaskEditModeOff(subtaskContainer);
+        }
+
+
+    })
+
+    if (subtaskInput.value !== "") {
+        subtasks.append(subtaskContainer);
+
+        localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML)
+    }
+
+}
+
+
+function subtaskEditModeOn(subtaskContainer) {
+    const subtaskText = subtaskContainer.querySelector("p");
+    const subtaskTextContent = subtaskContainer.querySelector("p").textContent;
+
+    const subtaskEditButton = subtaskContainer.querySelector(".edit-subtask-button");
+
+    subtaskText.innerHTML = `
+            <input value="${subtaskTextContent}" />
+        `
+    subtaskEditButton.textContent = "OK?";
+
+    localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML)
+}
+
+
+
+function subtaskEditModeOff(subtaskContainer) {
+
+    const subtaskEditButton = subtaskContainer.querySelector(".edit-subtask-button");
+
+    subtaskEditButton.textContent = "Edytuj";
+
+    subtaskContainer.querySelector("p").innerHTML = subtaskContainer.querySelector("input").value;
+
+    const subtaskTitle = subtaskContainer.querySelector(".subtask-title");
+    subtaskTitle.textContent = subtaskContainer.querySelector("p").textContent;
+
+
+    localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML)
+}
+
+
+
+
+
+
 
 
 function taskEditModeOn(taskContainer) {
@@ -84,7 +233,12 @@ function taskEditModeOff(taskContainer) {
     const taskEditButton = taskContainer.querySelector(".edit-task-button");
 
     taskEditButton.textContent = "Edytuj";
+
     taskContainer.querySelector("p").innerHTML = taskContainer.querySelector("input").value;
+
+    const taskTitle = taskContainer.querySelector(".task-title");
+    taskTitle.textContent = taskContainer.querySelector("p").textContent;
+
 
     localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML)
 }
@@ -118,9 +272,42 @@ removeTaskListener()
 
 
 
+function openTaskListener() {
+    const tasks = elements.mainTasksContainer.querySelectorAll(".task");
+    tasks.forEach(task => task.querySelector(".open-task-button").addEventListener("click", () => {
+
+        const modal = task.querySelector("dialog");
+        const closeModalButton = task.querySelector("dialog .close");
+
+        modal.setAttribute("open", "true");
+
+        closeModalButton.addEventListener("click", () => {
+            modal.removeAttribute("open");
+        })
+
+    }))
+}
+
+openTaskListener()
+
+
+
+/** Po odtworzeniu strony w pamięci znajduje się dialog z wartością "open", dlatego modale domyślnie się wyświetlają **/
+
+    document.querySelectorAll("dialog").forEach(dialog => {
+        dialog.removeAttribute("open")
+})
+
+    /** Fajne mogłoby być zrobienie zapisu do pamięci przy każdym zamknięciu modala i jego otwarciu **/
+/** END ROZKMINA **/
+
+
+
 function editTaskListener() {
     const tasks = elements.mainTasksContainer.querySelectorAll(".task");
     tasks.forEach(task => task.querySelector(".edit-task-button").addEventListener("click", () => {
+
+        const taskInputData = task.querySelector(".task-input-data");
 
         if (task.querySelector(".edit-task-button").dataset.isEdited === "false") {
             task.querySelector(".edit-task-button").dataset.isEdited = "true";
@@ -134,3 +321,91 @@ function editTaskListener() {
 }
 
 editTaskListener()
+
+function editSubtaskListener() {
+    const subtasks = elements.mainTasksContainer.querySelectorAll(".subtask");
+    subtasks.forEach(subtask => subtask.querySelector(".edit-subtask-button").addEventListener("click", () => {
+
+
+        const taskInputData = subtask.querySelector(".subtask-input-data");
+
+        if (subtask.querySelector(".edit-subtask-button").dataset.isEdited === "false") {
+            subtask.querySelector(".edit-subtask-button").dataset.isEdited = "true";
+            subtaskEditModeOn(subtask);
+        } else if (subtask.querySelector(".edit-subtask-button").dataset.isEdited === "true") {
+            subtask.querySelector(".edit-subtask-button").dataset.isEdited = "false";
+            subtaskEditModeOff(subtask);
+        }
+    }))
+}
+
+editSubtaskListener()
+
+
+// To przecież (chyba) duplikat!
+function closeSubtaskModalListener() {
+    const taskDialogs = elements.mainTasksContainer.querySelectorAll(".task dialog");
+
+    taskDialogs.forEach(taskDialog => taskDialog.querySelector(".close").addEventListener("click", () => {
+
+        taskDialog.removeAttribute("open");
+
+    }));
+}
+
+closeSubtaskModalListener()
+
+
+
+function clearSubtaskModalListener() {
+    const taskDialogs = elements.mainTasksContainer.querySelectorAll(".task dialog");
+
+    taskDialogs.forEach(taskDialog => taskDialog.querySelector(".clear-subtasks-data").addEventListener("click", () => {
+
+        taskDialog.querySelector(".subtasks").innerHTML = "";
+        localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML);
+    }));
+}
+
+clearSubtaskModalListener()
+
+
+
+
+function removeSubtaskButton() {
+
+    document.querySelectorAll(".subtasks").forEach(subtasksContainer => {
+
+        subtasksContainer.querySelectorAll(".subtask").forEach(subtaskElement => {
+            const removeSubtaskButton = subtaskElement.querySelector(".remove-subtask-button");
+
+            removeSubtaskButton.addEventListener("click", () => {
+                subtaskElement.remove();
+                localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML)
+            })
+        })
+
+    })
+}
+
+removeSubtaskButton();
+
+
+
+function addSubtaskButtonListener() {
+    const taskDialogs = elements.mainTasksContainer.querySelectorAll(".task dialog");
+
+    taskDialogs.forEach(taskDialog => taskDialog.querySelector(".add-subtask-button").addEventListener("click", () => {
+
+        const subtaskInput = taskDialog.querySelector(".subtask-input-field");
+        const addSubtaskButton = taskDialog.querySelector(".add-subtask-button");
+        const clearSubtasksData = taskDialog.querySelector(".clear-subtasks-data");
+        const subtasks = taskDialog.querySelector(".subtasks");
+
+        createSubtaskComponent(subtaskInput, addSubtaskButton, clearSubtasksData, subtasks)
+
+        localStorage.setItem("tasksData", elements.mainTasksContainer.innerHTML);
+    }));
+}
+
+addSubtaskButtonListener()
